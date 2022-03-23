394ac9e0bd6145cb0b3fb34bc58ee10ed2d7c59a
temelehtone <temelehtone@gmail.com>
2022-03-23 20:29:20 +0200

    setup

--- server/index.js
+++ server/index.js
@@ -3,20 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://temelehtone:sportapp@cluster0.q4tjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -25,5 +22,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((err) => console.log(err.message));
-
-
