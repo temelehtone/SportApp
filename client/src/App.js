3a8df16f88735865da4f72f95aed8331edfd4953
temelehtone <temelehtone@gmail.com>
2022-03-26 20:34:08 +0200

    site setups

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Route, Routes} from "react-router-dom"
 
-// import ResponsiveAppBar from "./components/NavBar/NavBar";
-import {getPosts} from "./actions/posts"
+import NavBar from "./components/NavBar";
+import Login from "./components/Login"
+import HomePage from "./components/HomePage"
+import SignUp from "./components/SignUp"
+import { getPosts } from "./actions/posts";
 
 const App = () => {
-  const [data, setData] = useState("")
-  
-  useEffect(()=> {
-    getPosts().then((data) => setData(data))
-  }, [])
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    
+  }, []);
   return (
-    <Container>
-      <button onClick={() => {console.log(data.message)}}></button>
-      
-    </Container>
+    <>
+      <NavBar user={user}/>
+
+      <Routes>
+          <Route exact path="/" element={<HomePage user={user}/>}/>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />}/>
+
+
+          <Route path="/products" element={<h1>Test</h1>}/>
+        </Routes>
+    </>
   );
 };
 
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@material-ui/core'
+
+const HomePage = ({user}) => {
+    const navigate = useNavigate()
+
+  return (
+    <div>
+       HOme
+    </div>
+  )
+}
+
+export default HomePage
